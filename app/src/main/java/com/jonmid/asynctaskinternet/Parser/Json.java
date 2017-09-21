986aa54e4288495a39b8d64d26f52a2c56a29179
ivandiaz2425 <ivandiaz2422@gmail.com>
2017-09-21 13:05:29 -0500

    2

--- app/src/main/java/com/jonmid/asynctaskinternet/Parser/Json.java
+++ app/src/main/java/com/jonmid/asynctaskinternet/Parser/Json.java
@@ -1,8 +1,38 @@
 package com.jonmid.asynctaskinternet.Parser;
 
+import com.jonmid.asynctaskinternet.Models.Comment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by aula7 on 21/09/17.
  */
 
 public class Json {
+
+    public static List<Comment> getdata(String content) throws JSONException {
+
+        JSONArray jsonArray = new JSONArray(content);
+        List<Comment> commentList = new ArrayList<>();
+
+        for (int i=0; i < jsonArray.length(); i++){
+            JSONObject item =jsonArray.getJSONObject(i);
+
+            Comment comment = new Comment();
+
+            comment.setId(item.getInt("id"));
+            comment.setEmail(item.getString("email"));
+            comment.setBody(item.getString("body"));
+
+            commentList.add(comment);
+        }
+        return commentList;
+
+    }
+
 }
